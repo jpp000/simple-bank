ff6da1503f9da261a369721fbefaed841fb08253
João Pedro <albuquerquejoaopedro04@gmail.com>
2026-06-09 11:39:44 -0300

    test: enhance user creation tests with password hashing validation

--- util/password_test.go
+++ util/password_test.go
@@ -20,4 +20,9 @@ func TestPassword(t *testing.T) {
 	wrongPassword := RandomString(6)
 	err = CheckPassword(wrongPassword, hashedPassword)
 	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+	require.NotEqual(t, hashedPassword, hashedPassword2)
 }
